3e6c0ca6253aa0fe7a7a2b7fc73e0f0ccb2f0a98
Phonekham <30440409+Phonekham@users.noreply.github.com>
2024-02-17 15:08:32 +0700

    setup clerk for user management

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://literate-crawdad-51.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
---
+++ providers/convex-client-providers.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+import { Loading } from "@/components/auth/loading";
+
+interface ConvexClientProvderProps {
+  children: React.ReactNode;
+}
+
+const clerkPulihableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+const convex = new ConvexReactClient(convexUrl);
+
+export const ConvexClientProvider = ({
+  children,
+}: ConvexClientProvderProps) => {
+  return (
+    <ClerkProvider publishableKey={clerkPulihableKey}>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        <Authenticated>{children}</Authenticated>
+        <AuthLoading>
+          <Loading />
+        </AuthLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
