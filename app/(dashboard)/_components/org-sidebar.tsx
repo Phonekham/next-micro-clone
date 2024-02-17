80bcfce9b6189e381027c9b8d0cd9f901b1bab1a
Phonekham <30440409+Phonekham@users.noreply.github.com>
2024-02-17 16:02:56 +0700

    dashboard & layout

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OrgSidebar = () => {
+  return (
+    <div className="hidden lg:flex flex-col space-y-6 w-[260px] pl-5 pt-5 bg-red-500">
+      OrgSidebar
+    </div>
+  );
+};
+
+export default OrgSidebar;
