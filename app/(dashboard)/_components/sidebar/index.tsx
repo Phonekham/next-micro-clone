a70946efa41d2684bcad4a7a5a8a04e3bb4feee6
Phonekham <30440409+Phonekham@users.noreply.github.com>
2024-02-17 17:19:55 +0700

    sidebar

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-
+import List from './list';
+import NewButton from './new-button'
 const Sidebar = () => {
   return (
-    <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4">
-      Sidebar
+    <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white">
+      <List/>
+      <NewButton/>
     </aside>
   );
 };
